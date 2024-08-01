43c4b1f61bae1cf0bacc55aab6c0eca79de8c717
Sean Kennedy <javacertsk@gmail.com>
2024-08-01 16:39:28 +0100

    2nd commit

---
+++ src/com/example/collections_lists_maps/sorting/BankApp.java
@@ -0,0 +1,34 @@
+package com.example.collections_lists_maps.sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BankApp {
+
+    public static void main(String[] args) {
+        // Create a list to hold BankAccount objects
+        List<BankAccount> accounts = new ArrayList<>();
+
+        // Add BankAccount objects to the list
+        accounts.add(new BankAccount("123456", 1000.0));
+        accounts.add(new BankAccount("654321", 2000.0));
+        accounts.add(new BankAccount("111111", 1500.0));
+        accounts.add(new BankAccount("222222", 500.0));
+
+        // Print the list of accounts before sorting
+        System.out.println("Accounts before sorting:");
+        for (BankAccount account : accounts) {
+            System.out.println(account);
+        }
+
+        // Sort the list of accounts based on balance
+        Collections.sort(accounts);
+
+        // Print the list of accounts after sorting
+        System.out.println("\nAccounts after sorting:");
+        for (BankAccount account : accounts) {
+            System.out.println(account);
+        }
+    }
+}
